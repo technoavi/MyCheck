ec36d97dbc54ee6fd2c3264b58973f82acd4b09c
Avinash Srivastava <Avinash Srivastava@192.168.1.108>
2017-02-25 23:06:47 +0530

    frth commit

--- AgProEngg_POC/src/main/java/com/syntaxtree/agproengg/Test/Newtest.java
+++ AgProEngg_POC/src/main/java/com/syntaxtree/agproengg/Test/Newtest.java
@@ -1,8 +1,21 @@
 package com.syntaxtree.agproengg.Test;
 
+import org.eclipse.jdt.internal.compiler.util.Util.Displayable;
+
 public class Newtest {
+	public Newtest() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void show(){
+	new Newtest().display();
+		System.out.println("show");
+	}
 	public static void main(String[] args) {
 		System.out.println("main method to test");
+		show();
 	}
-
+public void display(){
+	System.out.println("scnd method ");
+	
+}
 }
